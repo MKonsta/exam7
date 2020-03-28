7c511492cfd7695b5325ecabdf3730081746423a
MKonsta <kostyan.exe@gmail.com>
2020-03-28 08:58:52 +0300

    foods model

---
+++ src/main/java/com/example/exam/model/Place.java
@@ -0,0 +1,50 @@
+package com.example.exam.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@Document(collection = "places")
+public class Place {
+
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private List<String> foods;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getFoods() {
+        return foods;
+    }
+
+    public void setFoods(List<String> foods) {
+        this.foods = foods;
+    }
+}
